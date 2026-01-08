9ab6fcf0623446328e599528b568d9aa519b6fe3
Prashant0106 <itsmeprashant0106@gmail.com>
2026-01-08 10:37:46 +0530

    Add Question 2 - Even  or Odd

---
+++ Conditions/Question No.2.js
@@ -0,0 +1,9 @@
+//check num is positive, negative and zero
+let num = -5;
+if(num > 0){
+  console.log(num + " is a positive number");
+}else if(num < 0){
+  console.log(num + " is a negative number");
+}else{
+  console.log(num + " is zero");
+}
